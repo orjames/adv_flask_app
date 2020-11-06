986e2d9452635076c7d9f2aa79501aa344bfc362
Owen James <orjames@berkeley.edu>
2020-11-05 17:13:00 -0800

    contant for messages

--- resources/user.py
+++ resources/user.py
@@ -1,3 +1,4 @@
+from resources.item import BLANK_ERROR
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
 from flask_jwt_extended import (
@@ -12,12 +13,16 @@ from models.user import UserModel
 from blacklist import BLACKLIST
 
 _user_parser = reqparse.RequestParser()
-_user_parser.add_argument(
-    "username", type=str, required=True, help="This field cannot be blank."
-)
-_user_parser.add_argument(
-    "password", type=str, required=True, help="This field cannot be blank."
-)
+_user_parser.add_argument("username", type=str, required=True, help=BLANK_ERROR)
+_user_parser.add_argument("password", type=str, required=True, help=BLANK_ERROR)
+
+BLANK_ERROR = "'{}' cannot be blank."
+CREATED_SUCCESSFULLY = "User created successfully."
+INVALID_CREDENTIALS = "Invalid credentials!"
+USER_ALREADY_EXISTS = "A user with that username already exists."
+USER_DELETED = "User deleted."
+USER_LOGGED_OUT = "User <id={}> successfully logged out."
+USER_NOT_FOUND = "User not found."
 
 
 class UserRegister(Resource):
@@ -25,12 +30,12 @@ class UserRegister(Resource):
         data = _user_parser.parse_args()
 
         if UserModel.find_by_username(data["username"]):
-            return {"message": "A user with that username already exists."}, 400
+            return {"message": USER_ALREADY_EXISTS}, 400
 
         user = UserModel(**data)
         user.save_to_db()
 
-        return {"message": "User created successfully."}, 201
+        return {"message": CREATED_SUCCESSFULLY}, 201
 
 
 class User(Resource):
@@ -43,16 +48,16 @@ class User(Resource):
     def get(cls, user_id: int):
         user = UserModel.find_by_id(user_id)
         if not user:
-            return {"message": "User not found."}, 404
+            return {"message": USER_NOT_FOUND}, 404
         return user.json(), 200
 
     @classmethod
     def delete(cls, user_id: int):
         user = UserModel.find_by_id(user_id)
         if not user:
-            return {"message": "User not found."}, 404
+            return {"message": USER_NOT_FOUND}, 404
         user.delete_from_db()
-        return {"message": "User deleted."}, 200
+        return {"message": USER_DELETED}, 200
 
 
 class UserLogin(Resource):
@@ -68,7 +73,7 @@ class UserLogin(Resource):
             refresh_token = create_refresh_token(user.id)
             return {"access_token": access_token, "refresh_token": refresh_token}, 200
 
-        return {"message": "Invalid credentials!"}, 401
+        return {"message": INVALID_CREDENTIALS}, 401
 
 
 class UserLogout(Resource):
@@ -77,7 +82,7 @@ class UserLogout(Resource):
         jti = get_raw_jwt()["jti"]  # jti is "JWT ID", a unique identifier for a JWT.
         user_id = get_jwt_identity()
         BLACKLIST.add(jti)
-        return {"message": "User <id={}> successfully logged out.".format(user_id)}, 200
+        return {"message": USER_LOGGED_OUT.format(user_id)}, 200
 
 
 class TokenRefresh(Resource):
