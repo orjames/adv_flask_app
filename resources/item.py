d3a50b0463528fb1ffb05ce50e842922f101e5e7
Owen James <orjames@berkeley.edu>
2020-11-05 17:26:42 -0800

    update all to classmethods

--- resources/item.py
+++ resources/item.py
@@ -23,14 +23,19 @@ class Item(Resource):
         "store_id", type=int, required=True, help=BLANK_ERROR.format("store_id")
     )
 
-    def get(self, name: str):
+    # making it a classmethod using the @ decorator, doesn't depend on the object that's calling it (aka self)
+    # classmethod vs static methods, classmethods give more inheritance benefits, and access to cls
+    @classmethod
+    def get(cls, name: str):
         item = ItemModel.find_by_name(name)
         if item:
             return item.json(), 200
         return {"message": ITEM_NOT_FOUND}, 404
 
+    # classmethod goes above @fresh_jwt_required
+    @classmethod
     @fresh_jwt_required
-    def post(self, name: str):
+    def post(cls, name: str):
         if ItemModel.find_by_name(name):
             return {"message": NAME_ALREADY_EXISTS.format(name)}, 400
 
@@ -45,15 +50,17 @@ class Item(Resource):
 
         return item.json(), 201
 
+    @classmethod
     @jwt_required
-    def delete(self, name: str):
+    def delete(cls, name: str):
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
             return {"message": ITEM_DELETED}, 200
         return {"message": ITEM_NOT_FOUND}, 404
 
-    def put(self, name: str):
+    @classmethod
+    def put(cls, name: str):
         data = Item.parser.parse_args()
 
         item = ItemModel.find_by_name(name)
--- resources/user.py
+++ resources/user.py
@@ -26,7 +26,8 @@ USER_NOT_FOUND = "User not found."
 
 
 class UserRegister(Resource):
-    def post(self):
+    @classmethod
+    def post(cls):
         data = _user_parser.parse_args()
 
         if UserModel.find_by_username(data["username"]):
@@ -61,7 +62,8 @@ class User(Resource):
 
 
 class UserLogin(Resource):
-    def post(self):
+    @classmethod
+    def post(cls):
         data = _user_parser.parse_args()
 
         user = UserModel.find_by_username(data["username"])
@@ -77,8 +79,9 @@ class UserLogin(Resource):
 
 
 class UserLogout(Resource):
+    @classmethod
     @jwt_required
-    def post(self):
+    def post(cls):
         jti = get_raw_jwt()["jti"]  # jti is "JWT ID", a unique identifier for a JWT.
         user_id = get_jwt_identity()
         BLACKLIST.add(jti)
@@ -86,8 +89,9 @@ class UserLogout(Resource):
 
 
 class TokenRefresh(Resource):
+    @classmethod
     @jwt_refresh_token_required
-    def post(self):
+    def post(cls):
         current_user = get_jwt_identity()
         new_token = create_access_token(identity=current_user, fresh=False)
         return {"access_token": new_token}, 200
